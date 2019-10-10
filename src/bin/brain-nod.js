cda4dd67989ff3eb940851e07799e294283ec9e8
fill1986 <fill1986@gmail.com>
2019-10-10 14:56:07 +0300

    fix linter

--- src/bin/brain-nod.js
+++ src/bin/brain-nod.js
@@ -1,22 +1,24 @@
 #! /usr/bin/env node
-import { greeting, getName, generateNumber, checkAnswer, engineGame, getNOD } from '../index';
+import {
+  checkAnswer, engineGame, generateNumber, getNOD, greeting,
+} from '../index';
 
 const readlineSync = require('readline-sync');
 
 
 const gameNode = (name) => {
   greeting('Find the greatest common divisor of given numbers.');
-  const iter = (numTry) =>{
-    if (numTry==3){return console.log(`Congratulation, ${name} `)}
-    let num1=generateNumber(99);
-    let num2=generateNumber(99);
-    let trueResult = getNOD (num1, num2);
+  const iter = (numTry) => {
+    if (numTry == 3) { return console.log(`Congratulation, ${name} `); }
+    const num1 = generateNumber(99);
+    const num2 = generateNumber(99);
+    const trueResult = getNOD(num1, num2);
     console.log(`Question: ${num1}  ${num2}  //${trueResult}`);
-    const answerUser = readlineSync.question(`Yuor answer: `);
-    checkAnswer (answerUser, trueResult, name);
-  return iter((trueResult==answerUser)?numTry+1:0);
-  }
-return iter (0);
-}
+    const answerUser = readlineSync.question('Yuor answer: ');
+    checkAnswer(answerUser, trueResult, name);
+    return iter((trueResult == answerUser) ? numTry + 1 : 0);
+  };
+  return iter(0);
+};
 
-engineGame (gameNode);
+engineGame(gameNode);
--- src/bin/brain-progres.js
+++ src/bin/brain-progres.js
@@ -7,35 +7,41 @@ const readlineSync = require('readline-sync');
 
 const progresGamme = (name) => {
   greeting('What number is missing in the progression?');
-  let startProgression = generateNumber(60);
-  const stepPregression = generateNumber(10);
-  const arrProgression = [];
-  const endProg = startProgression + stepPregression * (10 - 1);
+  const iterTry = (numTry) => {
+    if (numTry == 3) { return console.log(`Congratulation, ${name} `); }
+    let startProgression = generateNumber(60);
+    const stepPregression = generateNumber(10);
+    const arrProgression = [];
+    const endProg = startProgression + stepPregression * (10 - 1);
 
-  for (startProgression; startProgression < endProg;) {
-    startProgression += stepPregression;
-    arrProgression.push(startProgression);
-  }
+    for (startProgression; startProgression < endProg;) {
+      startProgression += stepPregression;
+      arrProgression.push(startProgression);
+    }
 
-  const lostPosition = generateNumber(9);
-  const trueResult = arrProgression[lostPosition];
+    const lostPosition = generateNumber(9);
+    const trueResult = arrProgression[lostPosition];
 
-  const createQuestion = () => {
-    let stringQuestion = '';
-    const iter = (count) => {
-      if (count === arrProgression.length) {
-        return stringQuestion;
-      }
-      stringQuestion += (count !== lostPosition) ? ` ${arrProgression[count]}` : ' ..';
-      return iter(count + 1);
+    const createQuestion = () => {
+      let stringQuestion = '';
+      const iter = (count) => {
+        if (count === arrProgression.length) {
+          return stringQuestion;
+        }
+        stringQuestion += (count !== lostPosition) ? ` ${arrProgression[count]}` : ' ..';
+        return iter(count + 1);
+      };
+      return iter(0);
     };
-    return iter(0);
+
+    console.log(`Question:  ${createQuestion()}`);
+    const answerUser = readlineSync.question('Answer:');
+
+    checkAnswer(answerUser, trueResult, name);
+
+    return iterTry((trueResult == answerUser) ? numTry + 1 : 0);
   };
-
-  console.log(`Question:  ${createQuestion()}`);
-  const answerUser = readlineSync.question('Answer:');
-
-  checkAnswer(answerUser, trueResult, name);
+  iterTry(0);
 };
 
 engineGame(progresGamme);
