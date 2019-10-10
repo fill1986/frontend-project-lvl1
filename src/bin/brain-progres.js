d64bb1514407ce8ada8163d106b4c36729086578
fill1986 <fill1986@gmail.com>
2019-10-10 14:36:41 +0300

    add v1 Game with Progression

---
+++ src/bin/brain-progres.js
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+import {
+  checkAnswer, engineGame, generateNumber, greeting,
+} from '../index';
+
+const readlineSync = require('readline-sync');
+
+const progresGamme = (name) => {
+  greeting('What number is missing in the progression?');
+  let startProgression = generateNumber(60);
+  const stepPregression = generateNumber(10);
+  const arrProgression = [];
+  const endProg = startProgression + stepPregression * (10 - 1);
+
+  for (startProgression; startProgression < endProg;) {
+    startProgression += stepPregression;
+    arrProgression.push(startProgression);
+  }
+
+  const lostPosition = generateNumber(9);
+  const trueResult = arrProgression[lostPosition];
+
+  const createQuestion = () => {
+    let stringQuestion = '';
+    const iter = (count) => {
+      if (count === arrProgression.length) {
+        return stringQuestion;
+      }
+      stringQuestion += (count !== lostPosition) ? ` ${arrProgression[count]}` : ' ..';
+      return iter(count + 1);
+    };
+    return iter(0);
+  };
+
+  console.log(`Question:  ${createQuestion()}`);
+  const answerUser = readlineSync.question('Answer:');
+
+  checkAnswer(answerUser, trueResult, name);
+};
+
+engineGame(progresGamme);
